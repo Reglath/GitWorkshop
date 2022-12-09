a39c49a8c547830732c620ac0ba67b2073e8a69f
Jan Svoboda <jan.svob.jan@gmail.com>
2022-12-09 11:02:38 +0100

    c1

---
+++ SkeletonProject/SkeletonProject/JanCheer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkeletonProject
+{
+    public class JanCheer
+    {
+        public void Cheer()
+        {
+            Console.WriteLine("Tribes (M)");
+        }
+    }
+}
